899dd61210181b78374b373aba8f37b4de87ac7c
Lamu Guo <lamuguo@gmail.com>
2024-05-22 13:33:31 -0700

    temp 1

--- src/pizzaro/gateway.py
+++ src/pizzaro/gateway.py
@@ -1,18 +1,16 @@
 import json
+import logging
 
 
 def send_command(command, socket):
-    # 构建请求
     request = json.dumps({"command": command}).encode('utf-8')
 
-    # 发送请求
+    print('REQ: ', request)
     socket.sendall(request)
 
-    # 接收响应
     response = socket.recv(1024)
 
-    print("response: ", response)
+    print("RESP: ", response)
 
-    # 解析响应
     resp_obj = json.loads(response.decode('utf-8'))
     return resp_obj
